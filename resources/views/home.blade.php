ab28d1126b279e60b9314b1a54583e3f2a0e8e7e
James Roper <vadremix@gmail.com>
2017-06-11 12:50:39 -0700

    Add hero component to dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,28 @@
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
+    <header>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-12 brand">
+                    @if (env('APP_NAME_SEG1') !== null && env('APP_NAME_SEG2') !== null)
+                        <h1>{{ env('APP_NAME_SEG1') }}<span class="brand-secondary">{{ env('APP_NAME_SEG2') }}</span> <small>Cryptocurrency Dashboard</small></h1>
+                    @else
+                        <h1>{{ env('APP_NAME') }} <small>Cryptocurrency Dashboard</small></h1>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 hero-sub hero-sub-1">
+                    0 USD (Total Value)
+                </div>
+                <div class="col-sm-6 hero-sub hero-sub-2">
+                    <a href="#">Suggest Feature</a>
+                </div>
+            </div>
+        </div>
+    </header>
 
-<script src="/js/app.js" rel="script"></script>
+    <script src="/js/app.js" rel="script"></script>
 </body>
 </html>
