b513034d13d62a9c28d6d5be59283d8f0ef0c636
James Roper <vadremix@gmail.com>
2017-06-10 17:56:42 -0700

    Inject coin data service into api controller

--- app/Http/Controllers/CoinDataApiController.php
+++ app/Http/Controllers/CoinDataApiController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Classes\CoinDataService;
 use App\Models\CoinDataCodes;
 use Illuminate\Http\Request;
 
 class CoinDataApiController extends Controller
 {
+    protected $coinDataService;
+
+    public function __construct(CoinDataService $coinDataService)
+    {
+        $this->coinDataService = $coinDataService;
+    }
+
     public function list()
     {
 
