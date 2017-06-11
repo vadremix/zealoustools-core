b429eca90287765e46656ccf85d4e3a334e72cd7
James Roper <vadremix@gmail.com>
2017-06-10 17:18:21 -0700

    Make coin collections flat

--- app/Models/CoinDataCodes.php
+++ app/Models/CoinDataCodes.php
@@ -7,15 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 class CoinDataCodes extends Model
 {
     protected $hidden = ['id', 'coin_data_name_id', 'coin_data_provider_id'];
-    protected $with = ['name', 'provider'];
+    protected $appends = ['name', 'provider'];
 
-    public function name()
+    public function nameRelation()
     {
         return $this->belongsTo('App\Models\CoinDataNames', 'coin_data_name_id');
     }
 
-    public function provider()
+    public function providerRelation()
     {
         return $this->belongsTo('App\Models\CoinDataProviders', 'coin_data_provider_id');
+    }
+
+    public function getNameAttribute()
+    {
+        return $this->nameRelation()->first()->name;
+    }
+
+    public function getProviderAttribute()
+    {
+        return $this->providerRelation()->first()->name;
     }
 }
