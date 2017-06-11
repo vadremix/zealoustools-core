7d43e1202dd39417f604a48de004fc9deddcfd4a
James Roper <vadremix@gmail.com>
2017-06-11 10:26:44 -0700

    Create home view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Cryptocurrency Dashboard : {{ env('APP_NAME') }}</title>
+
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+
+<script src="/js/app.js" rel="script"></script>
+</body>
+</html>
