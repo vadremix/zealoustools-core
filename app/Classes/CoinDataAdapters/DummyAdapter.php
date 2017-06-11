9b73dd779e799abdad62b9cb694bff634f28a468
James Roper <vadremix@gmail.com>
2017-06-10 18:38:49 -0700

    Create fake coin data service adapter

---
+++ app/Classes/CoinDataAdapters/DummyAdapter.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Classes\CoinDataAdapters;
+
+class DummyAdapter extends CoinDataAdapterAbstract
+{
+    public function getPrice(string $coinCode)
+    {
+        return mt_rand(0, 3000);
+    }
+
+    protected function cacheLayer()
+    {
+        // fake implementation
+    }
+
+    protected function getData()
+    {
+        // fake implementation
+    }
+}
--- app/Classes/CoinDataService.php
+++ app/Classes/CoinDataService.php
@@ -12,4 +12,11 @@ class CoinDataService
     {
         $this->cacheRepository = $cacheRepository;
     }
+
+    public function createProvider(string $provider)
+    {
+        $providerClass = 'App\Classes\CoinDataAdapters\\' . $provider . 'Adapter';
+
+        return new $providerClass($this->cacheRepository);
+    }
 }
