b94602f4bcecd5b521fabc3234257421a232f650
James Roper <vadremix@gmail.com>
2017-06-12 10:10:32 -0700

    Refine hero unit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,19 +13,25 @@
     <header>
         <div class="container-fluid">
             <div class="row">
-                <div class="col-sm-12 brand">
+                <div class="col-xs-12 brand">
                     @if (env('APP_NAME_SEG1') !== null && env('APP_NAME_SEG2') !== null)
-                        <h1>{{ env('APP_NAME_SEG1') }}<span class="brand-secondary">{{ env('APP_NAME_SEG2') }}</span> <small>Cryptocurrency Dashboard</small></h1>
+                        <h1>{{ env('APP_NAME_SEG1') }}<span class="brand-secondary">{{ env('APP_NAME_SEG2') }}</span> <small>Cryptocurrency&nbsp;Dashboard</small></h1>
                     @else
                         <h1>{{ env('APP_NAME') }} <small>Cryptocurrency Dashboard</small></h1>
                     @endif
                 </div>
             </div>
             <div class="row">
-                <div class="col-sm-6 hero-sub hero-sub-1">
-                    0 USD (Total Value)
+                <div class="col-xs-4 col-sm-2 hero-sub hero-sub-1">
+                    Value:
                 </div>
-                <div class="col-sm-6 hero-sub hero-sub-2">
+                <div class="col-xs-4 col-sm-3 hero-sub hero-sub-1">
+                    0 BTC
+                </div>
+                <div class="col-xs-4 col-sm-3 hero-sub hero-sub-1">
+                    0 USD
+                </div>
+                <div class="col-xs-12 col-sm-4 hero-sub hero-sub-2">
                     <a href="#">Suggest Feature</a>
                 </div>
             </div>
