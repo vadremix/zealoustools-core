d1d86ad80e8d3a647c85b9d4f012255ae9604ffc
James Roper <vadremix@gmail.com>
2017-06-17 09:50:36 -0700

    Make trackers calculate value

--- resources/assets/js/components/Tracker.vue
+++ resources/assets/js/components/Tracker.vue
@@ -4,8 +4,8 @@
             <div class="tracker-heading">{{ coin.name + ' (' + coin.code + ')' }}</div>
             <div class="tracker-subtext">BTC: 1</div>
             <div class="tracker-subtext">USD: {{ coin.price }}</div>
-            <div class="tracker-subtext">Apx. Value: 0</div>
-            <input class="tracker-input" type="number" value="0">
+            <div class="tracker-subtext">Apx. Value: {{ value() }}</div>
+            <input :class="{ 'tracker-input' : true, 'input-danger': hasError }" type="number" v-model="amount" value="0">
             <div class="tracker-note">Source: {{ coin.provider }}</div>
         </div>
     </transition>
@@ -17,8 +17,23 @@
 
         data: function() {
             return {
-                seen: this.$props.visible
+                seen: this.$props.visible,
+                amount: 0,
+                hasError: false
             }
+        },
+
+        methods: {
+            value: function() {
+                console.log(this.amount);
+                if(!isNaN(this.amount)) {
+                    this.hasError = false;
+                    return this.amount * this.coin.price;
+                } else {
+                    this.hasError = true;
+                    return 0;
+                }
+            },
         },
 
         mounted: function() {
