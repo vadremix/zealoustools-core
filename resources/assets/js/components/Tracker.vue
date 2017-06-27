f7676ab7694a8d2350f31e40e2f871a8871d0e00
James Roper <vadremix@gmail.com>
2017-06-26 19:26:48 -0700

    Add tracker options menu

--- resources/assets/js/components/Tracker.vue
+++ resources/assets/js/components/Tracker.vue
@@ -1,12 +1,21 @@
 <template>
     <transition name="fade">
         <div v-if="seen" class="tracker">
-            <div class="tracker-heading">{{ coin.name + ' (' + coin.code + ')' }}</div>
+            <div class="tracker-heading">
+                {{ coin.name + ' (' + coin.code + ')' }}
+                <span class="pull-right tracker-options-button" :class="{ 'tracker-options-open': optionsToggle }" @click="toggleOptions()"><i class="glyphicon glyphicon-chevron-down"></i></span>
+            </div>
             <div class="tracker-subtext">BTC: 1</div>
             <div class="tracker-subtext">USD: {{ coin.price }}</div>
             <div class="tracker-subtext">Apx. Value: {{ value() }}</div>
             <input :class="{ 'tracker-input' : true, 'input-danger': hasError }" type="number" v-model="amount" value="0">
             <div class="tracker-note">Source: {{ coin.provider }}</div>
+
+            <div v-if="optionsToggle" class="tracker-options">
+                <ul>
+                    <li>Delete Tracker</li>
+                </ul>
+            </div>
         </div>
     </transition>
 </template>
@@ -19,7 +28,8 @@
             return {
                 seen: this.$props.visible,
                 amount: this.coin.amount,
-                hasError: false
+                hasError: false,
+                optionsToggle: false,
             }
         },
 
@@ -45,6 +55,14 @@
                 returnValue = returnValue[0].toLocaleString('en-US', { style: 'currency', currency: 'usd' });
                 return returnValue;
             },
+
+            toggleOptions() {
+                if(this.optionsToggle === true) {
+                    this.optionsToggle = false;
+                } else {
+                    this.optionsToggle = true;
+                }
+            }
         },
 
         watch: {
