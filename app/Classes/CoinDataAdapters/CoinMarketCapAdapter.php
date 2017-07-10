c3af08ff9116f990a87e7f38efcdd59f4a5b46f5
James Roper <vadremix@gmail.com>
2017-07-10 12:48:45 -0700

    Add CoinMarketCap adapter

---
+++ app/Classes/CoinDataAdapters/CoinMarketCapAdapter.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Classes\CoinDataAdapters;
+
+class CoinMarketCapAdapter extends CoinDataAdapterAbstract
+{
+    public function getPrice(String $coinCode)
+    {
+        // TODO: Implement getPrice() method.
+    }
+
+    public function cacheLayer()
+    {
+        // TODO: Implement cacheLayer() method.
+    }
+
+    public function getData()
+    {
+        // TODO: Implement getData() method.
+    }
+}
